74b2e1838a0f8f8958b1bab6305b03b6935b51a7
kennethreitz <me@kennethreitz.com>
2009-12-18 01:31:40 -0500

    darn syntax

--- plugin.php
+++ plugin.php
@@ -11,7 +11,7 @@
 
 $dir = WP_PLUGIN_URL.'/'.str_replace(basename( __FILE__),"",plugin_basename(__FILE__));
 
-<?php function clippy($text='copy-me'): ?>
+function clippy($text='copy-me'): ?>
     <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"
             width="110"
             height="14"
